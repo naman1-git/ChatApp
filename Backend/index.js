7446877811b81183210ab093c9e887c85a19ec03
naman1-git <namanadlakha2004@gmail.com>
2025-04-02 18:16:19 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -1,37 +1,42 @@
+// index.js - modified for Vercel
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
-import { app, server } from "./SocketIO/server.js";
 
 dotenv.config();
+const app = express();
 
-// middlewarenp
+// Connect to MongoDB only when handling requests
+let cachedDb = null;
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+  
+  const connection = await mongoose.connect("mongodb+srv://namanadlakha2004:namanadlakha@cluster0.3b5ae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+  cachedDb = connection;
+  return connection;
+}
+
+// Middleware
 app.use(cors({ origin: true, credentials: true }));
-
-
 app.use(express.json());
 app.use(cookieParser());
 
-const PORT = process.env.PORT ;
-
-mongoose.connect("mongodb+srv://namanadlakha2004:namanadlakha@cluster0.3b5ae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error("MongoDB connection error:", err));
-
-
-//routes
+// Routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
+  res.send("Server is running");
 });
 
-server.listen(PORT, () => {
-    console.log(`Server is Running on port ${PORT}`);
-});
+// Important for Vercel - use this pattern instead of server.listen()
+export default async (req, res) => {
+  await connectToDatabase();
+  return app(req, res);
+};
