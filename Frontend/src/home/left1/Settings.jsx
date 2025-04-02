6f6caddd4ec435f84129c429aa57027e323b48b7
naman1-git <namanadlakha2004@gmail.com>
2025-04-02 20:10:11 +0530

    .

--- Frontend/src/home/left1/Settings.jsx
+++ Frontend/src/home/left1/Settings.jsx
@@ -54,7 +54,7 @@ function Settings(user) {
         console.log("Profile Pic URL Set: ", uploadedImage.url);
 
         // Save the new profile picture URL to the database
-        await axios.put("https://chatapp-1-7iuz.onrender.com/api/user/update-profile_pic", { profile_pic: uploadedImage.url });
+        await axios.post("https://chatapp-1-7iuz.onrender.com/api/user/update-profile_pic", { profile_pic: uploadedImage.url });
       } else {
         toast.error("Failed to retrieve uploaded image URL");
       }
