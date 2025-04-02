be3d0f1758a255b1aab9ed3240ac0faa6d795aef
naman1-git <namanadlakha2004@gmail.com>
2025-04-02 20:20:53 +0530

    .

--- Frontend/src/home/left1/Settings.jsx
+++ Frontend/src/home/left1/Settings.jsx
@@ -40,7 +40,7 @@ function Settings(user) {
     try {
       console.log("Uploading file to Cloudinary...");
       const res = await fetch("https://api.cloudinary.com/v1_1/ddm7nxdwd/image/upload", {
-        method: "POST",
+        method: "PUT",
         body: data,
       });
       
@@ -54,7 +54,7 @@ function Settings(user) {
         console.log("Profile Pic URL Set: ", uploadedImage.url);
 
         // Save the new profile picture URL to the database
-        await axios.post("https://chatapp-1-7iuz.onrender.com/api/user/update-profile_pic", { profile_pic: uploadedImage.url });
+        await axios.put("https://chatapp-1-7iuz.onrender.com/api/user/update-profile_pic", { profile_pic: uploadedImage.url });
       } else {
         toast.error("Failed to retrieve uploaded image URL");
       }
