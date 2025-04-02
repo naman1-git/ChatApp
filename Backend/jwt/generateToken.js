2a575979e7b2a10de8bbc7fdfc64ed690267949c
naman1-git <namanadlakha2004@gmail.com>
2025-04-03 02:31:00 +0530

    .

--- Backend/jwt/generateToken.js
+++ Backend/jwt/generateToken.js
@@ -1,14 +1,13 @@
+import jwt from "jsonwebtoken";
+
 const createTokenAndSaveCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
     expiresIn: "10d",
   });
-
   res.cookie("jwt", token, {
-    httpOnly: true, 
-    secure: process.env.NODE_ENV === "production", // Ensures secure cookies in production
-    sameSite: "None", // REQUIRED for cross-origin cookies
+    httpOnly: true, // xss
+    secure: true,
+    sameSite: "none", // csrf
   });
-
-  res.status(200).json({ success: true, message: "Logged in successfully" }); // Send response
 };
-
+export default createTokenAndSaveCookie;
