3e77817b4ff27697e2cbdb1cc791dd6f4a50ff8c
naman1-git <namanadlakha2004@gmail.com>
2025-04-02 13:31:10 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT ;
 const URI = process.env.MONGODB_URI;
 
 try {
