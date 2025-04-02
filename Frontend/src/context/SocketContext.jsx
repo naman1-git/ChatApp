a1ebea4e2f1f1487ac4931bf2cacf6d4cf2b5aed
naman1-git <namanadlakha2004@gmail.com>
2025-04-02 20:00:32 +0530

    .

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -1,30 +1,32 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useAuth } from "./AuthProvider";
 import io from "socket.io-client";
-const socketContext = createContext();
 
-// it is a hook.
+const SocketContext = createContext();
+
+// Hook to access socket context
 export const useSocketContext = () => {
-  return useContext(socketContext);
+  return useContext(SocketContext);
 };
 
 export const SocketProvider = ({ children }) => {
+  const { user: authUser } = useAuth();
   const [socket, setSocket] = useState(null);
   const [onlineUsers, setOnlineUsers] = useState([]);
-  const [authUser] = useAuth();
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chatapp-1-7iuz.onrender.com", {
-        query: {
-          userId: authUser.user._id,
-        },
+      const newSocket = io("https://chatapp-1-7iuz.onrender.com", {
+        query: { userId: authUser._id },
       });
-      setSocket(socket);
-      socket.on("getOnlineUsers", (users) => {
+
+      setSocket(newSocket);
+
+      newSocket.on("getOnlineUsers", (users) => {
         setOnlineUsers(users);
       });
-      return () => socket.close();
+
+      return () => newSocket.close();
     } else {
       if (socket) {
         socket.close();
@@ -32,9 +34,10 @@ export const SocketProvider = ({ children }) => {
       }
     }
   }, [authUser]);
+
   return (
-    <socketContext.Provider value={{ socket, onlineUsers }}>
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
       {children}
-    </socketContext.Provider>
+    </SocketContext.Provider>
   );
 };
