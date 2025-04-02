2d9833b1f7fb87e08c34cf66198a3e8b72910cea
naman1-git <namanadlakha2004@gmail.com>
2025-04-02 18:37:10 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -1,53 +1,37 @@
-// api/index.js (main entry point for Vercel)
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import userRoute from "../routes/user.route.js";
-import messageRoute from "../routes/message.route.js";
+
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
 
 dotenv.config();
 
-// Create Express app
-const app = express();
-
-// Middleware
+// middlewarenp
 app.use(cors({ origin: true, credentials: true }));
+
+
 app.use(express.json());
 app.use(cookieParser());
 
-// Connection pooling for MongoDB
-let cachedDb = null;
-async function connectToDatabase() {
-  if (cachedDb) {
-    return cachedDb;
-  }
-  
-  try {
-    const connection = await mongoose.connect("mongodb+srv://namanadlakha2004:namanadlakha@cluster0.3b5ae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-      serverSelectionTimeoutMS: 5000, // Reduce timeout to 5 seconds
-    });
-    
-    cachedDb = connection;
-    console.log("Connected to MongoDB");
-    return connection;
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-    throw err;
-  }
-}
+const PORT = process.env.PORT ;
 
-// Routes
+mongoose.connect("mongodb+srv://namanadlakha2004:namanadlakha@cluster0.3b5ae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+
+//routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
 app.get("/", (req, res) => {
-  res.status(200).send("hello from server");
+    res.send("Server is running");
 });
 
-// Connect to database
-connectToDatabase().catch(console.error);
-
-// Export the Express API
-export default app;
+server.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
+});
